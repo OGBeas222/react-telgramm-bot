2d79dd88c58067cbd415919fdadacc1a11ba9bf2
OGBeas222 <zholudev111@gmail.com>
2022-10-03 16:29:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const tgAPI = window.Telegram.WebApp;
+
+  useEffect(()=>{
+    tgAPI.ready();
+  },[])
+
+
+  const onClose = ()=>{
+    tgAPI.close();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>hello telegram</h1>
+        <button onClick={onClose()}>Close</button>
+      </div>
     </div>
   );
 }
